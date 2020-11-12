Project-Awaken/android_vendor_awaken

awaken: Introduce Flipendo ( Extreme Battery Saver )
Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

config/common.mk
@@ -50,7 +50,8 @@ endif
 # copy privapp permissions
 PRODUCT_COPY_FILES += \
     vendor/awaken/prebuilt/common/etc/permissions/privapp-permissions-awaken-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-awaken-product.xml \
-    vendor/awaken/prebuilt/common/etc/permissions/privapp-permissions-awaken-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-awaken-system.xml
+    vendor/awaken/prebuilt/common/etc/permissions/privapp-permissions-awaken-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-awaken-system.xml \
+    vendor/awaken/prebuilt/common/etc/sysconfig/pixel_experience_2020.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_experience_2020.xml
 
 # system mount
 PRODUCT_COPY_FILES += \
@@ -91,6 +92,9 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
+# Inherit from sepolicy config
+$(call inherit-product, vendor/awaken/config/sepolicy.mk)
 
 # Fonts
 include vendor/awaken/config/fonts.mk

config/sepolicy.mk
@@ -1,0 +1,18 @@
+#
+# Copyright (C) 2020 Raphielscape LLC. and Haruka LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+BOARD_SEPOLICY_DIRS += \
+	vendor/awaken/sepolicy/vendor

land-msm-3-18/platform_vendor_aosp

config/common.mk
@@ -127,6 +127,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Exchange2
 
+# Flipendo
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/sysconfig/pixel_experience_2020.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_experience_2020.xml
+
+
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml
@@ -179,3 +184,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Recommend using the non debug dexpreopter
 USE_DEX2OAT_DEBUG := false
+
+# Inherit from sepolicy config
+$(call inherit-product, vendor/awaken/config/sepolicy.mk)

config/sepolicy.mk
@@ -1,0 +1,18 @@
+#
+# Copyright (C) 2020 Raphielscape LLC. and Haruka LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+BOARD_SEPOLICY_DIRS += \
+	vendor/aosp/sepolicy/vendor